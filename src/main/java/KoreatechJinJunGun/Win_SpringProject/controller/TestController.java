7e5a5d4c1031fb7f3e680f9ed0634d81fa09a283
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-01-19 17:06:50 +0900

    feat: 테스트 코드 추가

--- src/main/java/KoreatechJinJunGun/Win_SpringProject/controller/TestController.java
+++ src/main/java/KoreatechJinJunGun/Win_SpringProject/controller/TestController.java
@@ -1,4 +1,20 @@
 package KoreatechJinJunGun.Win_SpringProject.controller;
 
+import KoreatechJinJunGun.Win_SpringProject.entity.TestEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
 public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<TestEntity> test(@RequestBody TestEntity kk){
+        log.info("test controller");
+        return ResponseEntity.status(HttpStatus.OK).body(kk);
+    }
 }
--- src/main/java/KoreatechJinJunGun/Win_SpringProject/entity/TestEntity.java
+++ src/main/java/KoreatechJinJunGun/Win_SpringProject/entity/TestEntity.java
@@ -1,4 +1,9 @@
 package KoreatechJinJunGun.Win_SpringProject.entity;
 
+import lombok.Getter;
+
+@Getter
 public class TestEntity {
+    int id;
+    String data;
 }
