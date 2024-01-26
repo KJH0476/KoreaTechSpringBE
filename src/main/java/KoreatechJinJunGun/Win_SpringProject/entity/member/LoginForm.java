9cdec56a9567baa927bb60a18632163ca2619269
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-01-26 15:38:26 +0900

    chore: validation 어노테이션 변경
    @NotNull -> @NotBlank

--- src/main/java/KoreatechJinJunGun/Win_SpringProject/entity/member/LoginForm.java
+++ src/main/java/KoreatechJinJunGun/Win_SpringProject/entity/member/LoginForm.java
@@ -1,13 +1,13 @@
 package KoreatechJinJunGun.Win_SpringProject.entity.member;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LoginForm {
 
-    @NotNull(message = "이메일을 입력해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
     String email;
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     String password;
 }
