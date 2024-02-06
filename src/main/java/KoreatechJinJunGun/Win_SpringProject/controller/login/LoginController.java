e6c66e6ba8ad87b241fa0cc0690b5f52d5ebb3e2
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-02-06 21:21:21 +0900

    refactor: 응답 바디 메세지 생성 메소드 추출 리팩토링

--- src/main/java/KoreatechJinJunGun/Win_SpringProject/controller/login/LoginController.java
+++ src/main/java/KoreatechJinJunGun/Win_SpringProject/controller/login/LoginController.java
@@ -1,7 +1,9 @@
 package KoreatechJinJunGun.Win_SpringProject.controller.login;
 
 import KoreatechJinJunGun.Win_SpringProject.entity.member.LoginForm;
+import KoreatechJinJunGun.Win_SpringProject.entity.member.token.TokenDto;
 import KoreatechJinJunGun.Win_SpringProject.service.login.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -10,10 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -32,8 +31,6 @@ public class LoginController {
         //이메일, 비밀번호 입력하지 않았을 때 예외 처리
         if(bindingResult.hasErrors()){
             List<FieldError> errors = bindingResult.getFieldErrors();
-            //에러 메세지를 담을 map
-            Map<String, String> errorMessage = new ConcurrentHashMap<>();
             StringBuilder sb = new StringBuilder();
 
             for (FieldError error : errors) {
@@ -41,25 +38,20 @@ public class LoginController {
                 sb.append(" ");
             }
             sb.append("입력해주세요.");
-            errorMessage.put("errorMessage", sb.toString());
 
             //바디에 에러메세지 담아서 반환, 400 에러
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(makeResponseBody(sb.toString()), HttpStatus.BAD_REQUEST);
         }
 
         //로그인 성공 로직
-        String token = loginService.login(form);
+        TokenDto token = loginService.login(form);
 
-        //응답 헤더에 토큰을 넣어줌
+        //응답 헤더에 토큰을 넣어줌(최초 로그인시 액세스토큰 + 갱신토큰 둘다 발급)
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token);
-
-        // 응답 바디에 메세지 담아서 반환
-        Map<String, String> body = new ConcurrentHashMap<>();
-        body.put("message", "success login");
+        headers.add("Authorization", "Bearer " + token.getAccessToken());
 
         //로그인 성공시 200 반환
-        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+        return new ResponseEntity<>(makeResponseBody("success login"), headers, HttpStatus.OK);
     }
 
     @GetMapping("/admin/test")
@@ -72,4 +64,10 @@ public class LoginController {
         return "user success";
     }
 
+    //응답 바디 생성
+    private static Map<String, String> makeResponseBody(String bodyMessage){
+        Map<String, String> body = new ConcurrentHashMap<>();
+        body.put("message", bodyMessage);
+        return body;
+    }
 }
